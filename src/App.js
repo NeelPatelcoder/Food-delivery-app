02e5705554acfb2bcbb4f4d896a20841c7c678a5
NeelPatelcoder <neelp.mobio@gmail.com>
2021-07-28 18:17:15 +0530

    Header

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './Components/Header'
+import Resturent from './Components/Resturent';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Resturent />
+    </div>
+  );
+}
+
+export default App;
